79aec6592bbf47b519a3a4a18f96206fe8cf4740
Nodari Nakhutsrishvili <Nakhutsrishvili@yandex.ru>
2023-09-04 23:41:35 +0300

    express-mesto-api-full-gha-15pr

--- backend/routes/users.js
+++ backend/routes/users.js
@@ -27,8 +27,8 @@ router.patch(
   '/me',
   celebrate({
     body: Joi.object().keys({
-      name: Joi.string().min(2).max(30),
-      about: Joi.string().min(2).max(30),
+      name: Joi.string().min(2).max(30).required(),
+      about: Joi.string().min(2).max(30).required(),
     }),
   }),
   editUserData,
@@ -38,7 +38,7 @@ router.patch(
   '/me/avatar',
   celebrate({
     body: Joi.object().keys({
-      avatar: Joi.string().pattern(urlRegex),
+      avatar: Joi.string().pattern(urlRegex).required(),
     }),
   }),
   editUserAvatar,
